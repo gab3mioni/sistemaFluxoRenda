3a76d601ed96501a35a777d6f20a9e97c39bef3b
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-10-22 12:31:50 -0300

    Estabelecendo init_database

--- database/init_database.sql
+++ database/init_database.sql
@@ -1,2 +1,70 @@
 CREATE DATABASE sistemaFluxoRenda;
 USE sistemaFluxoRenda;
+
+CREATE TABLE IF NOT EXISTS governo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_transacao ENUM('imposto', 'beneficio') NOT NULL,
+    descricao VARCHAR(255),
+    valor DECIMAL(15, 2) NOT NULL,
+    data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS empresas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    receita DECIMAL(15, 2),
+    despesa DECIMAL(15, 2),
+    investimento DECIMAL(15, 2),
+    imposto DECIMAL(15, 2),
+    beneficio_governo DECIMAL(15, 2),
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS familias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    renda DECIMAL(15, 2),
+    consumo DECIMAL(15, 2),
+    investimento DECIMAL(15, 2),
+    beneficio_governo DECIMAL(15, 2),
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE IF NOT EXISTS setor_externo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_transacao ENUM('exportacao', 'importacao') NOT NULL,
+    descricao VARCHAR(255),
+    valor DECIMAL(15, 2) NOT NULL,
+    data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS setor_financeiro (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_transacao ENUM('investimento', 'poupanca', 'emprestimo') NOT NULL,
+    valor DECIMAL(15, 2) NOT NULL,
+    origem ENUM('familia', 'empresa', 'governo', 'setor_externo') NOT NULL,
+    destino ENUM('familia', 'empresa', 'governo', 'setor_externo') NOT NULL,
+    data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS transacao_familia_empresa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_familia INT,
+    id_empresa INT,
+    valor DECIMAL(15, 2) NOT NULL,
+    tipo_transacao ENUM('consumo', 'salario') NOT NULL,
+    data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_familia) REFERENCES familias(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_empresa) REFERENCES empresas(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS transacao_governo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_familia INT NULL,
+    id_empresa INT NULL,
+    valor DECIMAL(15, 2) NOT NULL,
+    tipo_transacao ENUM('imposto', 'beneficio') NOT NULL,
+    data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_familia) REFERENCES familias(id) ON DELETE SET NULL,
+    FOREIGN KEY (id_empresa) REFERENCES empresas(id) ON DELETE SET NULL
+);
