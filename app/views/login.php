61b7dfd27f12c0aa56726a062ddd592d9b624cd2
MelissaMaximo <129124909+MelissaMaximo@users.noreply.github.com>
2024-10-25 00:17:47 -0300

    Criando tela de login
    Criando tela de login com css3 e bootstrap.

---
+++ app/views/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="../../public/assets/css/login/login.css" rel="stylesheet">
+
+   
+</head>
+<body>
+
+    <div class="login-container">
+        <h2 class="text-center">Login</h2>
+
+        <form action="/login" method="POST">
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail:</label>
+                <input type="email" id="email" name="email" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="senha" class="form-label">Senha:</label>
+                <input type="password" id="senha" name="senha" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="tipo" class="form-label">Tipo de Acesso:</label>
+                <select id="tipo" name="tipo" class="form-select" required>
+                    <option value="governo">Governo</option>
+                    <option value="empresa">Empresa</option>
+                    <option value="familia">Família</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary w-100">Entrar</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
