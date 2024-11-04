76c38bdafbd95e293ec4ac9a340dfe01eb093dcc
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-11-04 00:45:35 -0300

    feature: Histórico de transações externas

--- app/helpers/HistoricoHelper.php
+++ app/helpers/HistoricoHelper.php
@@ -19,6 +19,20 @@ class HistoricoHelper
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public static function getTransacoesSetorExterno(PDO $pdo, string $tipo, int $id): array
+    {
+        $column = $tipo === 'familia' ? 'id_familia' : 'id_empresa';
+        $query = $pdo->prepare("
+        SELECT id AS id, NULL AS id_{$tipo}, tipo_transacao, descricao, valor, data_transacao
+        FROM setor_externo
+        WHERE $column = :id
+    ");
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     public static function getTransacoesSetorFinanceiro(PDO $pdo, string $tipo, int $id): array
     {
         $column = $tipo === 'familia' ? 'id_familia' : 'id_empresa';
@@ -47,9 +61,11 @@ class HistoricoHelper
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function combinarEOrdenarTransacoes(array $transacoesFamiliaEmpresa, array $transacoesSetorFinanceiro, array $transacoesGoverno): array
+    public static function combinarEOrdenarTransacoes(array $transacoesFamiliaEmpresa,
+                                                      array $transacoesSetorFinanceiro, array $transacoesGoverno, array $transacoesExterno): array
     {
-        $combinedResults = array_merge($transacoesFamiliaEmpresa, $transacoesSetorFinanceiro, $transacoesGoverno);
+        $combinedResults = array_merge($transacoesFamiliaEmpresa, $transacoesSetorFinanceiro,
+            $transacoesGoverno, $transacoesExterno);
 
         usort($combinedResults, function ($a, $b) {
             return strtotime($b['data_transacao']) - strtotime($a['data_transacao']);
